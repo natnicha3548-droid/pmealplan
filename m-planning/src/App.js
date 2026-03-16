89074120652344cb8b547b8b87b031dd070f7d89
Kanida_A <sb6640248101@lru.ac.th>
2026-03-16 14:42:41 +0700

    ทดสอบ

--- m-planning/src/App.js
+++ m-planning/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React
+          {/* React ยูหู้ */}
         </a>
       </header>
     </div>
